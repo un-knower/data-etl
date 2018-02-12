69c765be55e79a860954d999657d4950e2e7a570
zehendong <zehengdong@gmail.com>
2018-02-12 11:35:20 +0800

    [add] add common function

---
+++ src/main/scala/com/eric/utils/ConstantUtil.scala
@@ -0,0 +1,24 @@
+package com.eric.utils
+
+/**
+  * Created by dongzeheng on 2017/10/8.
+  */
+object ConstantUtil {
+  /**
+    * 字段名称
+    */
+  //车牌号码
+  val PLATE_NO = "plateno"
+  //车牌颜色
+  val PLATE_COLOR = "platecolor"
+  //车牌颜色 + 号码
+  val PLATE_COLOR_NO = "platecolor_no"
+
+
+  /** 车辆信息表 */
+  val BAYONET_VEHICLEPASS_Table = "BAYONET_VEHICLEPASS"
+  val vehicleInfoTablePropertyFamily = "cf"
+  val vehicleInfoTablePropertyColumn = "property"
+  val vehicleInfoTableCountFamily = "cfd"
+  val scanBatch = 500
+}
